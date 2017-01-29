2b7de5a8a938c6f947676167f1eeaafc6ea5fa73
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-01-29 18:42:50 +0100

    Using ejs as view engine

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 var http = require ('http');
 var express = require ('express');
+var ejsEngine = require ('ejs-locals');
 
 var app = express();
 var port = process.env.port || 3000;
 
 //Setup the View engine.
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+app.engine('ejs', ejsEngine);  //To support master pages
+app.set('view engine', 'ejs'); //Set ejs as view engine
 
 app.get ('/', function (request, response){
     //response.send("<html><body><h1>Hello</h1></body></html>");
-    response.render ('jade/index', {
+    /*response.render ('jade/index', {
         title: 'Express using jade'
+    });*/
+    response.render ('ejs/index', {
+        title: 'Express using ejs'
     });
 });
 
