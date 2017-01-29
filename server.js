7874988f5d026daa1ad2283667d899285496152b
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-01-29 18:12:51 +0100

    Installed jade as view engine.

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var http = require ('http');
+var express = require ('express');
+
+var app = express();
 var port = process.env.port || 3000;
 
+//Setup the View engine.
+app.set('view engine', 'jade');
+
+app.get ('/', function (request, response){
+    //response.send("<html><body><h1>Hello</h1></body></html>");
+    response.render ('jade/index', {
+        title: 'Express using jade'
+    });
+});
+
+app.get ('/api/users', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send ([{
+        name: 'Jhon',
+        isValid: true
+    },{
+        name: 'Pete',
+        isValid: true
+    }])
+});
 
 console.log('Starting  server...');
-var server = http.createServer(function (request, response) {
-
-    //console.log('Ctrl+C to end.');
-    response.writeHead( 200, { 'Content-Type' : 'text/plain'});
-    response.end('Hello World\n');
-});
+var server = http.createServer(app);
 
 server.listen(port);
