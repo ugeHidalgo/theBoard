20a21ca9abdf4bb735ee8c953714f58b6361f02f
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-01-29 19:08:36 +0100

    Use vash as view engine

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 var http = require ('http');
 var express = require ('express');
-var ejsEngine = require ('ejs-locals');
+//var ejsEngine = require ('ejs-locals');
 
 var app = express();
 var port = process.env.port || 3000;
 
 //Setup the View engine.
 //app.set('view engine', 'jade');
-app.engine('ejs', ejsEngine);  //To support master pages
-app.set('view engine', 'ejs'); //Set ejs as view engine
+//app.engine('ejs', ejsEngine);  //To support master pages
+//app.set('view engine', 'ejs'); //Set ejs as view engine
+app.set('view engine', 'vash'); //Set vash as view engine
+
 
 app.get ('/', function (request, response){
-    //response.send("<html><body><h1>Hello</h1></body></html>");
-    /*response.render ('jade/index', {
+    //response.send("<html><body><h1>Hello</h1></body></html>"); //Sin view engine.
+    /*response.render ('jade/index', {  //Using jade as view engine
         title: 'Express using jade'
-    });*/
-    response.render ('ejs/index', {
+    });
+    response.render ('ejs/index', { //using ejs as view engine
         title: 'Express using ejs'
+    });*/
+    response.render ('index', { //Using vash as view engine
+        title: 'Express using vash'
     });
 });
 
