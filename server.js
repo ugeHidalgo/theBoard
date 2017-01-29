4f4a27096c0f306507b4dc201991b8b31502c2c1
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-01-29 17:30:48 +0100

    Added express, and ignore node_modules golder in git.

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var http = require ('http');
 var port = process.env.port || 3000;
 
 
+console.log('Starting  server...');
 var server = http.createServer(function (request, response) {
+
+    //console.log('Ctrl+C to end.');
     response.writeHead( 200, { 'Content-Type' : 'text/plain'});
     response.end('Hello World\n');
 });
