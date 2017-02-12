0fbd587c16c253f3e36c4291ec2b4cef25bcf0ab
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-02-12 19:14:59 +0100

    Adde a new form to create notes, with logic in controller to save to DB.

--- public/src/notesView/notesView.js
+++ public/src/notesView/notesView.js
@@ -1,10 +1,12 @@
 (function (angular) {
-    var theModule = angular.module ('notesView', []); 
+    var theModule = angular.module ('notesView', ['ui.bootstrap']); 
     
      theModule.controller ('notesViewController',  [
          '$scope', '$window', '$http',
          function ($scope, $window, $http) {
             $scope.notes = [];
+            $scope.colors= ['yellow', 'blue', 'orange', 'green'];
+            $scope.newNote = createEmptyNote();
 
             //Get the category name
             var urlParts = $window.location.pathname.split('/');
@@ -22,7 +24,25 @@
                     alert (error);
                 });
 
+            $scope.saveNote = function () {
+                $http.post( notesUrl, $scope.newNote).
+                    then(function (result) {
+                        $scope.notes.push(result.data);
+                        $scope.newNote = createEmptyNote();
+                    }, function (error) {
+
+                    });  
+            };
+
         }
         ]);
+
+        function createEmptyNote() {
+            return {
+                color: 'yellow',
+                note: '',
+                author: ''
+            };
+        };
 
 })(window.angular);
